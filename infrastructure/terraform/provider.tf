3504ac1aab2a48c88022e0a23947c9be357487b6
jamiekariuki <jamiekariuki18@gmail.com>
2025-11-19 20:17:49 +0300

    feat: add kubernetese-alpha provider

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -33,3 +33,9 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
+
+provider "kubernetes-alpha" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
