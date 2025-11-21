fea2be7dc9dcf09e488fa36f71f31851925f8f52
jamiekariuki <jamiekariuki18@gmail.com>
2025-11-21 15:39:14 +0300

    fix: fix kubernetes providers

--- infrastructure/argo-cd/provider.tf
+++ infrastructure/argo-cd/provider.tf
@@ -3,35 +3,29 @@ provider "aws" {
   allowed_account_ids = [var.aws_account_id]
 }
 
-
 # Get cluster info
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-
-  depends_on = [module.eks]
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
 }
 
 # Get authentication token
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-
-  depends_on = [module.eks]
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
 }
 
 # Kubernetes provider
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
 }
 
 # Helm provider
 provider "helm" {
   kubernetes ={
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.default.token
   }
 }
 
